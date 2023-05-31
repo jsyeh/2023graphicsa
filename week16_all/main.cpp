0777e52c307a41b32f73bfb86de0ff585b6ded17
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-05-31 10:32:05 +0800

    week16

--- week16_all/main.cpp
+++ week16_all/main.cpp
@@ -1,7 +1,24 @@
-#include <GL/glut.h>
 #include <stdio.h>
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <GL/glut.h>
 #include "glm.h"
 GLMmodel * gundam = NULL;
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename); ///OpenCV讀圖
+    cvCvtColor(img,img, CV_BGR2RGB); ///OpenCV轉色彩 (需要cv.h)
+    glEnable(GL_TEXTURE_2D); ///1. 開啟貼圖功能
+    GLuint id; ///準備一個 unsigned int 整數, 叫 貼圖ID
+    glGenTextures(1, &id); /// 產生Generate 貼圖ID
+    glBindTexture(GL_TEXTURE_2D, id); ///綁定bind 貼圖ID
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖T, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖S, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); /// 貼圖參數, 放大時的內插, 用最近點
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); /// 貼圖參數, 縮小時的內插, 用最近點
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+    return id;
+}
 
 float teapotX=0, teapotY=0, angle=0, oldX=0, oldY=0;
 void mouse(int button, int state, int x, int y) {
@@ -20,9 +37,10 @@ void motion(int x, int y) {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glColor3f(1,0,0);
+        glColor3f(1,1,1);///glColor3f(1,0,0);
         glScalef(0.03, 0.03, 0.03);
-        glmDraw(gundam, GLM_MATERIAL);
+        glRotatef(angle, 0, 1, 0);
+        glmDraw(gundam, GLM_MATERIAL | GLM_TEXTURE);
     glPopMatrix();
 
     glColor3f(0,1,0);
@@ -41,6 +59,8 @@ int main(int argc, char** argv)
     glutMouseFunc(mouse);
 
     gundam = glmReadOBJ("model/gundam.obj");
+    myTexture("model/Diffuse.jpg");
+    glEnable(GL_DEPTH_TEST);
 
     glutMainLoop();
 }
